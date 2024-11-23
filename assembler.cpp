8437510be2ca2b199c1d0f68b3f51220bbc3a40f
Artem <jack.tag@mail.ru>
2024-11-23 14:30:16 +0300

    assembler and interpreter in deffirent files

---
+++ assembler.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <string>
+#include <cmath>
+#include <map>
+using namespace std;
+
+string error_to_test;
+
+void error_detected(string error_name){
+    error_to_test = error_name;
+    std::cerr<<error_name;
+    exit(EXIT_FAILURE);
+}
+
+string transformate(vector<int> length, int sum, vector<string> arguments){
+    string out="";
+    string str="";
+    string tek;
+    for (int i =0;i<3;i++){
+        tek = arguments[i];
+        bitset<32> x = stoi(tek);
+        tek = "";
+        for (int j = 0;j<length[i];j++)
+            tek+=to_string(x[j]);
+        //cout<<tek<<endl;
+        str+=tek;
+    }
+    int sz = str.size();
+    for (int i = 0;i<sum-sz;i++)
+        str+='0';
+    
+    for (int i =0;i<4;i++){
+        tek="";
+        int x = 0;
+        for (int j=(i+1)*8-1;j>=i*8+4;j--)
+            x+=int(str[j]-'0')*pow(2,j-i*8-4);
+        
+        if (x<10)    
+            out+="0x"+to_string(x);
+        else {
+            out+="0x";
+            out+=char(x-10+'A');
+        }
+        x=0;
+        for (int j=i*8+3;j>=i*8;j--)
+            x+=int(str[j]-'0')*pow(2,j-i*8);
+        
+        if (x<10)
+            out+=to_string(x);
+        else out+=char(x-10+'A');
+        out+=" ";
+    }
+
+    return out;
+}
+
+string log_in(vector<string> arguments,string out,string command){
+    string log = "";
+    log += "  <instruction>\n";
+    log += "    <mnemonic>"+command+"</mnemonic>\n";
+    log += "    <opcode>" +arguments[0]+"</opcode>\n";
+    log += "    <operand>"+arguments[1]+"</operand>\n";
+    log += "    <result_register>"+arguments[2]+"</result_register>\n";
+    log += "    <binary>"+out+"</binary>\n";
+    log += "  </instruction>\n";
+    return log;
+}
+
+void read_input(string path_to_log, string path_to_bin,string path_to_input){
+    vector<int> length;
+    ofstream log(path_to_log+"log.xml");
+    ofstream binFile(path_to_bin+"output.bin");
+    log << "<log>\n";
+    
+    ifstream fin(path_to_input+"input.txt");
+    string s = "";
+    while (getline(fin,s)){
+        s+=' ';
+        string command = s.substr(0,s.find(' '));
+        
+        s = s.substr(s.find(' ')+1,s.size()-s.find(' ')-1);
+        vector<string> arguments;
+        while (s.size()>0){
+            arguments.push_back(s.substr(0,s.find(' ')));
+            s = s.substr(s.find(' ')+1,s.size()-s.find(' ')-1);
+        }
+
+        if (command=="LOAD_CONST"){
+            length.push_back(4);
+            length.push_back(19);
+            length.push_back(3);
+            
+        }
+        else if (command=="READ_VALUE"){
+            length.push_back(4);
+            length.push_back(3);
+            length.push_back(15);
+            
+        }
+        else if (command=="WRITE_VALUE"){
+            length.push_back(4);
+            length.push_back(15);
+            length.push_back(3);
+             
+        }
+        else if (command=="BITREVERSE"){
+            length.push_back(4);
+            length.push_back(3);
+            length.push_back(3);
+        }
+        else error_detected("The undefined name of command!\n");
+        string out = transformate(length,32,arguments);
+        binFile<<out+"\n";
+        out = log_in(arguments,out,command);
+        log<<out;
+        length.clear();
+    }
+    
+    log << "</log>\n";
+    fin.close();
+    log.close();
+    binFile.close();
+}
---
+++ interpreter.cpp
@@ -0,0 +1,124 @@
+#include "assembler.cpp"
+using namespace std;
+
+map<int,int> memory,registers;
+int l,r;
+
+int bin_to_dec(string s){
+    int out = 0;
+    for (int i=0;i<s.size();i++)
+        if (s[i]=='1')
+            out+=pow(2,s.size()-i-1);
+    return out;
+}
+
+int bitreverse(int number,int length){
+    bitset<32> bits = number;
+    string s = "";
+    for (int i=0;i<length;i++)
+        s+=to_string(bits[i]);
+    return bin_to_dec(s);
+}
+
+
+void out_result(){
+    ofstream fout("result.xml");
+    fout << "<result>\n";
+    int pred=-1;
+
+    for (auto c:registers){
+        if (c.first>=l){
+            if (c.first>r)
+                break;
+            if (pred==-1)
+                for (int i = l;i<c.first;i++)
+                    fout<<"<register address="<<i<<">"<<-1<<"</register>\n";
+            else {                
+                for (int i =pred+1;i<min(c.first,r);i++)
+                    fout<<"<register address="<<i<<">"<<-1<<"</register>\n";
+             }
+            
+            fout<<"<register address="<<c.first<<">"<<c.second<<"</register>\n";
+            
+            pred = c.first;
+        }
+    }
+    
+    for (int i = max(pred+1,l);i<=r;i++)
+            fout<<"<register address="<<i<<">"<<-1<<"</register>\n";
+
+    fout << "<result>\n";
+    fout.close();
+}
+
+string string_reverse(string s){
+    int size = s.size();
+    string out = "";
+    for (int i = s.size()-1;i>=0;i--)
+        out+=s[i];
+    return out;
+}
+
+void interpreter(){
+    fstream binFile("output.bin");
+    string s;
+    while (getline(binFile,s)){
+        s+=" ";
+        vector<string> parts;
+        
+        while (s.size()>1){
+            parts.push_back(s.substr(2,s.find(' ')-2));
+            s = s.substr(s.find(' ')+1,s.size()-s.find(' ')-1);
+        }
+        
+        for (int i=0;i<4;i++)
+            swap(parts[i][0],parts[i][1]);
+        
+        string bit_str="";
+        for (auto c:parts){
+            for (int i =0;i<2;i++){
+                int x;
+                if (c[i]>='A' && c[i]<='F')
+                    x=10+c[i]-'A';
+                else x = c[i]-'0';
+                bitset<4> bits = x;
+                for (int j = 0;j<4;j++)
+                    bit_str+=to_string(bits[j]);
+            }
+        }
+        int command,b,c;
+        command = bin_to_dec(string_reverse(bit_str.substr(0,4)));
+        switch(command){
+            case 0: b = bin_to_dec(string_reverse(bit_str.substr(4,19)));
+                    c = bin_to_dec(string_reverse(bit_str.substr(23,3)));
+                    registers[c]=b;
+                    break;
+            case 15: b = bin_to_dec(string_reverse(bit_str.substr(4,3)));
+                    c = bin_to_dec(string_reverse(bit_str.substr(7,15)));
+                    if (memory[c]==0)
+                        error_detected("memory is empty!\n");
+                    registers[b]=memory[c];
+                    break;
+            case 7: b = bin_to_dec(string_reverse(bit_str.substr(4,15)));
+                    c = bin_to_dec(string_reverse(bit_str.substr(19,3)));
+                    if (registers[c]==0)
+                        error_detected("register is empty!\n");
+                    memory[b]=registers[c];
+                    break;
+            case 12: 
+                    b = bin_to_dec(string_reverse(bit_str.substr(4,3)));
+                    c = bin_to_dec(string_reverse(bit_str.substr(7,3)));
+                    if (registers[c]==0)
+                        error_detected("register is empty!\n");
+                    else if (memory[registers[c]]==0)
+                        error_detected("memory is empty!\n");
+                    else registers[b]=bitreverse(memory[registers[c]],3);
+                    break;
+            default: 
+                    error_detected("The unknown index of command!\n");
+                    break; 
+        }        
+    }
+    binFile.close();
+    out_result();
+}
